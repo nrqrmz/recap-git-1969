0797310318d4434845402e93e9392b79cfa0279f
Enrique Ramírez Ramos <nrqrmz@gmail.com>
2025-05-05 15:09:22 -0600

    Create load_customers.sql

---
+++ load_customers.sql
@@ -0,0 +1,9 @@
+SELECT
+ID
+, Year_of_birth
+, Registered_at__fidelity_app_
+, has_verified_email
+, Has_opt_out_phone
+, Order_in_4_weeks
+, Orders_count
+FROM `data-analytics-bootcamp-363212.bolk.customers`
