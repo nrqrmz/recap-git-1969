54de36594f38ff7c5c030d48e427d74bfd761c4b
VincentJinwooOh <ohjinwoo9696@gmail.com>
2025-05-05 17:33:00 -0400

    has_verified_phone orders_count

--- load_customers.sql
+++ load_customers.sql
@@ -1,9 +1,9 @@
 SELECT
 ID
-, CAST(Year_of_birth AS int64) AS year_clean -- James
+, Year_of_birth -- James
 , Registered_at__fidelity_app_ -- Jose
 , has_verified_email -- Ibrahim
-, Has_opt_out_phone -- Vincent
+, if(Has_verified_phone,1,0) as has_verified_phone
 , Order_in_4_weeks -- Ibrahim
-, Orders_count -- Vincent
+, ifnull(cast(Orders_count as int64),0) as Orders_count -- Vincent
 FROM `data-analytics-bootcamp-363212.bolk.customers`
