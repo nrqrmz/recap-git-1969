838fa366c2e8b1c6a9d079a4d4e441e1bd95ef9e
Jose <josedaro186@gmail.com>
2025-05-05 17:54:53 -0400

    corrected name

---
+++ load_customers.sql
@@ -0,0 +1,14 @@
+SELECT
+  ID
+  , Year_of_birth 
+  ,   
+  CASE 
+    WHEN Registered_at__fidelity_app_ = 'MAN'
+    THEN DATE(LEFT(Registered_by, 10))
+    ELSE DATE(LEFT(Registered_at__fidelity_app_, 10)) 
+  END AS date_date
+  , has_verified_email 
+  , Has_opt_out_phone 
+  , Order_in_4_weeks 
+  , Orders_count
+FROM `data-analytics-bootcamp-363212.bolk.customers`
