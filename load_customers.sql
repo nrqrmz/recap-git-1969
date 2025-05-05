d063e23ab4bb3c1a0ce8628abf9416614d6dd555
Jose <josedaro186@gmail.com>
2025-05-05 17:39:50 -0400

    get the dates in correct format

--- load_customers.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-ID
-, Year_of_birth -- James
-, Registered_at__fidelity_app_ -- Jose
-, has_verified_email -- Ibrahim
-, Has_opt_out_phone -- Vincent
-, Order_in_4_weeks -- Ibrahim
-, Orders_count -- Vincent
-FROM `data-analytics-bootcamp-363212.bolk.customers`
